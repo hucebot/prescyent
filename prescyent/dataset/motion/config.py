5d84350728d3313f24de73b0cda19943d16e883e
abiver <alexis.biver@inria.fr>
2023-01-20 14:45:52 +0100

    🐛 bump changes to the dataset's naming convention

--- prescyent/dataset/motion/config.py
+++ prescyent/dataset/motion/config.py
@@ -2,6 +2,6 @@ from prescyent.dataset.base_config import DatasetConfig
 
 
 class MotionDatasetConfig(DatasetConfig):
-    input_length = 10       # number of timesteps as input
-    output_length = 10      # number of predicted timesteps
+    input_size = 10       # number of timesteps as input
+    output_size = 10      # number of predicted timesteps
     # TODO see torch Dataset args for more
