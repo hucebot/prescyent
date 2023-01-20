fb6c610c0e85043d9a4aa051e18786eaa1cd82e5
abiver <alexis.biver@inria.fr>
2023-01-20 14:47:41 +0100

    🚸 bump old samples and add new ones

--- samples/lstm_baseline.py
+++ samples/lstm_baseline.py
@@ -1,6 +1,7 @@
-from prescyent.dataset.motion.teleop_icub.config import TeleopIcubDatasetConfig
+from prescyent.evaluator.metrics import get_ade, get_fde
+from prescyent.evaluator.plotting import plot_prediction
 from prescyent.predictor import LSTMPredictor, LSTMConfig, TrainingConfig
-from prescyent.dataset import TeleopIcubDataset
+from prescyent.dataset import TeleopIcubDataset, TeleopIcubDatasetConfig
 
 
 if __name__ == "__main__":
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     dataset = TeleopIcubDataset(dataset_config)
 
     # -- Init predictor
-    hidden_size=32
+    hidden_size=100
     feature_size = dataset.feature_size
     config = LSTMConfig(feature_size=feature_size,
                         output_size=output_size,
@@ -23,8 +24,15 @@ if __name__ == "__main__":
     predictor = LSTMPredictor(config=config)
 
     # Train, Test and Save
-    training_config = TrainingConfig(epoch=2)
+    training_config = TrainingConfig(epoch=5)
     predictor.train(dataset.train_dataloader, training_config)
     predictor.test(dataset.test_dataloader)
-    save_path = "data/example/lstm_ver1"
+    save_path = "data/example/lstm_baseline_ver1"
     predictor.save(save_path)
+    # plot some test episodes
+    input = dataset.test_datasample[0][0]
+    truth = dataset.test_datasample[0][1]
+    prediction = predictor(input)
+    plot_prediction(dataset.test_datasample[0], prediction, "data/eval/pred_data_1.png")
+    truth = dataset.test_datasample[0][1]
+    print("ADE: %.5f\nFDE: %.5f" % (get_ade(truth, prediction), get_fde(truth, prediction)))
