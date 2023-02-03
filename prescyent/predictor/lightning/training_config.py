e30bc99c3c8d3d652c7086075183f8df14af3f88
abiver <biveralexis@gmail.com>
2023-02-03 14:49:02 +0100

    🧑‍💻 better default values for TrainingConfig

--- prescyent/predictor/lightning/training_config.py
+++ prescyent/predictor/lightning/training_config.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 
 class TrainingConfig(BaseModel):
-    epoch: int = 50
-    accelerator: str = "gpu"
-    devices: Union[str, int] = 2
+    epoch: int = 100
+    accelerator: str = "auto"
+    devices: Union[str, int] = "auto"
     # TODO see pl trainer args for more
