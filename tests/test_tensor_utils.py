7516b72746323097400ef198b7263178aa7e6077
abiver <biveralexis@gmail.com>
2023-02-07 12:42:08 +0100

    ✅ test for tensor utils flatten function

---
+++ tests/test_tensor_utils.py
@@ -0,0 +1,20 @@
+import unittest
+import torch
+
+
+from prescyent.utils.tensor_manipulation import flatten_list_of_preds
+
+
+class TestFlattenListPreds(unittest.TestCase):
+
+    def test_return_as_is(self):
+        input_pred = torch.arange(3 * 3).view(3, 3)    
+        output_pred = flatten_list_of_preds(input_pred)
+        self.assertTrue(torch.equal(input_pred, output_pred))
+        
+    def test_flatten(self):
+        input_pred = torch.arange(3 * 3 * 3).view(3, 3, 3)
+        expected_output = torch.Tensor([[ 6.,  7.,  8.], [15., 16., 17.], [24., 25., 26.]])
+        output_pred = flatten_list_of_preds(input_pred)
+        self.assertTrue(torch.equal(expected_output, output_pred))
+        
