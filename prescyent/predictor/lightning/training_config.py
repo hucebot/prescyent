22aea9a73c40e1f29191138e8027e59e9ab05605
abiver <biveralexis@gmail.com>
2023-02-10 15:39:54 +0100

    🚨 fix lightning warning, default None is now -1

--- prescyent/predictor/lightning/training_config.py
+++ prescyent/predictor/lightning/training_config.py
@@ -7,6 +7,6 @@ from prescyent.predictor.lightning.optimizer_config import OptimizerConfig
 class TrainingConfig(OptimizerConfig):
     """Pydantic Basemodel for Pytorch Lightning Training configuration"""
     epoch: int = 100
-    max_steps: int = None
+    max_steps: int = -1
     accelerator: str = "auto"
     devices: Union[str, int] = "auto"
