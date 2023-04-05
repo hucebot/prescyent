43538394b80915269aa685e2c9460d0c04d606e1
abiver <biveralexis@gmail.com>
2023-04-05 17:27:41 +0200

    ⚡️ serialize the trajectories for motion samples instead of creating new tensor pairs

--- prescyent/dataset/datasamples.py
+++ prescyent/dataset/datasamples.py
@@ -1,20 +1,61 @@
 """Data pair of sample and truth for motion data in ML"""
-from torch import Tensor
+from typing import List
+from prescyent.dataset.config import LearningTypes
+
+from prescyent.dataset.trajectories import Trajectory
 
 
 class MotionDataSamples():
     """Class storing x,y pairs for ML trainings on motion data"""
-    sample: Tensor
-    truth: Tensor
-    len: int
+    trajectories: List[Trajectory]
 
-    def __init__(self, sample: Tensor, truth: Tensor) -> None:
-        self.sample = sample
-        self.truth = truth
-        self.len = self.sample.shape[0]
+    def __init__(self, trajectories: List[Trajectory],
+                 history_size: int, future_size: int,
+                 sampling_type: LearningTypes) -> None:
+        self.trajectories = trajectories
+        self.history_size = history_size
+        self.future_size = future_size
+        self.sampling_type = sampling_type
+        if self.sampling_type != LearningTypes.SEQ2SEQ and \
+                self.sampling_type != LearningTypes.AUTOREG:
+            raise NotImplementedError(f"We don't handle {self.sampling_type} sampling for now.")
+        self.sample_ids = self._map_to_flatten_trajs()
 
-    def __getitem__(self, index):
-        return self.sample[index], self.truth[index]
+    def _map_to_flatten_trajs(self):
+        map = {}
+        map_id = 0
+        if self.sampling_type == LearningTypes.SEQ2SEQ:
+            invalid_frames_per_traj = self.history_size + self.future_size
+        if self.sampling_type == LearningTypes.AUTOREG:
+            invalid_frames_per_traj = self.history_size + 1
+        for t, trajectory in enumerate(self.trajectories):
+            if len(trajectory) < invalid_frames_per_traj:
+                raise ValueError("Check that the intended history size and future size are compatible"
+                                 f" with the dataset. A trajectory of size {len(trajectory)} can't be"
+                                 f" split in samples of sizes {invalid_frames_per_traj}")
+            map.update({map_id + i: (t, i) for i in range(len(trajectory) - invalid_frames_per_traj)})
+            map_id += len(trajectory) - invalid_frames_per_traj
+        return map
+
+    def _get_item_seq2seq(self, index: int):
+        traj_id, tensor_id = self.sample_ids[index]
+        trajectory = self.trajectories[traj_id]
+        sample = trajectory[tensor_id:tensor_id + self.history_size]
+        truth = trajectory[tensor_id + self.history_size:tensor_id + self.history_size + self.future_size]
+        return sample, truth
+
+    def _get_item_autoreg(self, index: int):
+        traj_id, tensor_id = self.sample_ids[index]
+        trajectory = self.trajectories[traj_id]
+        sample = trajectory[tensor_id:tensor_id + self.history_size]
+        truth = trajectory[tensor_id + 1:tensor_id + self.history_size + 1]
+        return sample, truth
+
+    def __getitem__(self, index: int):
+        if self.sampling_type == LearningTypes.SEQ2SEQ:
+            return self._get_item_seq2seq(index)
+        if self.sampling_type == LearningTypes.AUTOREG:
+            return self._get_item_autoreg(index)
 
     def __len__(self):
-        return self.len
+        return len(self.sample_ids)
--- prescyent/dataset/dataset.py
+++ prescyent/dataset/dataset.py
@@ -29,15 +29,21 @@ class MotionDataset(Dataset):
         logger.debug("Tensor pairs will be generated for a %s learning type",
                         self.config.learning_type,
                         group=DATASET)
-        self.train_datasample = self._make_datasample(self.trajectories.train)
-        logger.info("Generated %d tensor pairs for training", len(self.train_datasample),
-                    group=DATASET)
-        self.test_datasample = self._make_datasample(self.trajectories.test)
-        logger.info("Generated %d tensor pairs for testing", len(self.test_datasample),
-                    group=DATASET)
-        self.val_datasample = self._make_datasample(self.trajectories.val)
-        logger.info("Generated %d tensor pairs for validation", len(self.val_datasample),
-                    group=DATASET)
+        self.train_datasample = MotionDataSamples(self.trajectories.train,
+                                                  history_size=self.history_size,
+                                                  future_size=self.future_size,
+                                                  sampling_type=self.config.learning_type)
+        logger.info("Train dataset has a size of %d", len(self.train_datasample))
+        self.test_datasample = MotionDataSamples(self.trajectories.test,
+                                                  history_size=self.history_size,
+                                                  future_size=self.future_size,
+                                                  sampling_type=self.config.learning_type)
+        logger.info("Test dataset has a size of %d", len(self.test_datasample))
+        self.val_datasample = MotionDataSamples(self.trajectories.val,
+                                                  history_size=self.history_size,
+                                                  future_size=self.future_size,
+                                                  sampling_type=self.config.learning_type)
+        logger.info("Val dataset has a size of %d", len(self.val_datasample))
 
     @property
     def train_dataloader(self):
@@ -50,17 +56,16 @@ class MotionDataset(Dataset):
     @property
     def test_dataloader(self):
         return DataLoader(self.test_datasample, batch_size=self.batch_size,
-                          shuffle=False, num_workers=self.config.num_workers,
+                          shuffle=False, num_workers=1,
                           pin_memory=self.config.pin_memory,
-                          drop_last=self.config.drop_last,
-                          persistent_workers=self.config.persistent_workers)
+                          drop_last=False, persistent_workers=False)
 
     @property
     def val_dataloader(self):
         return DataLoader(self.val_datasample, batch_size=self.batch_size,
                           shuffle=False, num_workers=self.config.num_workers,
                           pin_memory=self.config.pin_memory,
-                          drop_last=self.config.drop_last,
+                          drop_last=False,
                           persistent_workers=self.config.persistent_workers)
 
     @property
@@ -74,12 +79,6 @@ class MotionDataset(Dataset):
     @property
     def feature_size(self):
         return self.num_dims * self.num_points
-
-    def __getitem__(self, index):
-        return self.val_datasample[index]
-
-    def __len__(self):
-        return len(self.val_datasample)
 
     def _init_from_config(self,
                           config: Union[Dict, None, str, Path, MotionDatasetConfig],
@@ -116,55 +115,6 @@ class MotionDataset(Dataset):
         with save_path.open('w', encoding="utf-8") as conf_file:
             logger.debug(self.config.dict(), group=DATASET)
             json.dump(self.config.dict(), conf_file, indent=4, sort_keys=True)
-
-    def _make_datasample(self, trajectories):
-        sample = torch.FloatTensor([])   # shape(num_sample, seq_len, num_point, num_dim)
-        truth = torch.FloatTensor([])
-        if self.config.learning_type == LearningTypes.SEQ2SEQ:
-            tensor_pair_function = self._make_seq2seq_pairs
-        elif self.config.learning_type == LearningTypes.AUTOREG:
-            tensor_pair_function = self._make_autoreg_pairs
-        else:
-            raise NotImplementedError(f"Learning type {self.config.learning_type}"
-                                        " is not implemented yet")
-        for trajectory in trajectories:
-            sample_trajectory, truth_trajectory = tensor_pair_function(trajectory)
-            sample = torch.cat([sample, sample_trajectory], dim=0)
-            truth = torch.cat([truth, truth_trajectory], dim=0)
-        return MotionDataSamples(sample, truth)
-
-    # This could use padding to get recognition from the first time-steps
-    def _make_seq2seq_pairs(self, trajectory):
-        if len(trajectory) < self.history_size + self.future_size:
-            raise ValueError("Check that the intended history size and future size are compatible"
-                             f" with the dataset. A trajectory of size {len(trajectory)} can't be"
-                             f" split in samples of sizes {self.history_size}"
-                             f" and {self.future_size}")
-        sample = [trajectory[i:i + self.history_size]
-                  for i in range(len(trajectory) - self.history_size - self.future_size + 1)]
-        truth = [trajectory[i + self.history_size:i + self.history_size + self.future_size]
-                 for i in range(len(trajectory) - self.history_size - self.future_size + 1)]
-        # -- use the stack function to convert the list of 1D tensors
-        # into a 2D tensor where each element of the list is now a row
-        sample = torch.stack(sample)
-        truth = torch.stack(truth)
-        return sample, truth
-
-    # This could use padding to get recognition from the first time-steps
-    def _make_autoreg_pairs(self, trajectory):
-        if len(trajectory) < self.history_size + 1:
-            raise ValueError("Check that the intended history size and future size are compatible"
-                             f" with the dataset. A trajectory of size {len(trajectory)} can't be"
-                             f" split in samples of sizes {self.history_size} + 1")
-        sample = [trajectory[i:i + self.history_size]
-                  for i in range(len(trajectory) - self.history_size)]
-        truth = [trajectory[i + 1:i + self.history_size + 1]
-                 for i in range(len(trajectory) - self.history_size)]
-        # -- use the stack function to convert the list of 1D tensors
-        # into a 2D tensor where each element of the list is now a row
-        sample = torch.stack(sample)
-        truth = torch.stack(truth)
-        return sample, truth
 
     def _download_files(self, url, path):
         """get the dataset files from an url"""
