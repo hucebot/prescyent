6639b3e6733bccf30b1798d46a7d24b152eb46ac
abiver <biveralexis@gmail.com>
2023-05-11 11:21:17 +0200

    ✨ add variable amount of layer for SARLSTM

--- prescyent/predictor/lightning/models/autoreg/sarlstm/module.py
+++ prescyent/predictor/lightning/models/autoreg/sarlstm/module.py
@@ -17,9 +17,9 @@ class TorchModule(BaseTorchModule):
         super().__init__(config)
         self.hidden_size = config.hidden_size
         self.feature_size = config.feature_size
-
-        self.lstm1 = nn.LSTMCell(self.feature_size, self.hidden_size)
-        self.lstm2 = nn.LSTMCell(self.hidden_size, self.hidden_size)
+        self.num_layers = config.num_layers
+        self.lstms = nn.ModuleList([nn.LSTMCell(self.feature_size, self.hidden_size)])
+        self.lstms.extend([nn.LSTMCell(self.hidden_size, self.hidden_size) for i in range(self.num_layers - 1)])
         self.linear = nn.Linear(self.hidden_size, self.feature_size)
 
     @BaseTorchModule.allow_unbatched
@@ -32,28 +32,27 @@ class TorchModule(BaseTorchModule):
         # input shape is (batch_size, seq_len, num_feature)
         batch_size = T[0]
         # init the hidden states
-        h1 = torch.zeros(batch_size, self.hidden_size, device=input_tensor.device)
-        c1 = torch.zeros(batch_size, self.hidden_size, device=input_tensor.device)
-        h2 = torch.zeros(batch_size, self.hidden_size, device=input_tensor.device)
-        c2 = torch.zeros(batch_size, self.hidden_size, device=input_tensor.device)
+        hs = [torch.zeros(batch_size, self.hidden_size, device=input_tensor.device) for i in range(self.num_layers)]
+        cs = [torch.zeros(batch_size, self.hidden_size, device=input_tensor.device) for i in range(self.num_layers)]
 
-        # encoding
         for input_frame in input_tensor.split(1, dim=1):
             # input_frame shape is (batch_size, 1, num_feature)
             # the lstmcell is called for each item of the sequence
             # we want (batch_size, 1, num_feature) => (batch_size, num_feature)
-            input_frame = torch.squeeze(input_frame, 1)
-            h1, c1 = self.lstm1(input_frame, (h1, c1))
-            h2, c2 = self.lstm2(h1, (h2, c2))
-            prediction = self.linear(h2)
+            next_input = torch.squeeze(input_frame, 1)
+            for i in range(self.num_layers):
+                hs[i], cs[i] = self.lstms[i](next_input, (hs[i], cs[i]))
+                next_input = hs[i]
+            prediction = self.linear(next_input)
             predictions.append(torch.unsqueeze(prediction, 1))
 
-        # decoding
-        # we loop over the layers for each output desired
+
         for _ in range(future_size - 1):
-            h1, c1 = self.lstm1(prediction, (h1, c1))
-            h2, c2 = self.lstm2(h1, (h2, c2))
-            prediction = self.linear(h2)
+            next_input = prediction
+            for i in range(self.num_layers):
+                hs[i], cs[i] = self.lstms[i](next_input, (hs[i], cs[i]))
+                next_input = hs[i]
+            prediction = self.linear(next_input)
             # reshape to (batch_size, 1, num_feature)
             predictions.append(torch.unsqueeze(prediction, 1))
 
