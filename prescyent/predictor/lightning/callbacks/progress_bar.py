f14deac01a6133b7404efac1615339827592f463
abiver <biveralexis@gmail.com>
2023-10-02 08:42:46 +0200

    🐛 fix progress bar changes in lightning v2

--- prescyent/predictor/lightning/callbacks/progress_bar.py
+++ prescyent/predictor/lightning/callbacks/progress_bar.py
@@ -10,9 +10,7 @@ class LightningProgressBar(TQDMProgressBar):
 
     def on_train_epoch_start(self, trainer, *_) -> None:
         super().on_train_epoch_start(trainer, *_)
-        bar = super().init_train_tqdm()
         max_epoch = trainer.max_epochs
         if max_epoch < 0:
             max_epoch = calculate_max_epoch(trainer)
-        bar.set_description(f"Epoch {trainer.current_epoch + 1}/{max_epoch}")
-        return bar
+        self.train_progress_bar.set_description(f"Epoch {trainer.current_epoch}/{max_epoch}")
