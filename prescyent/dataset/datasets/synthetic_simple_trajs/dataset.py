✨ add Normalization as a Scaler, with tests ✨ Load and save a scaler, also from predictor loading and saving 🐛 fix seed in SSTDataset

prescyent/dataset/datasets/synthetic_simple_trajs/dataset.py
@@ -1,7 +1,6 @@
 """Class and methods for the SST Dataset
 https://zenodo.org/record/5913573#.Y75xK_7MIaw
 """
-import random
 from pathlib import Path
 from typing import Union, Dict, List
 
@@ -45,6 +44,7 @@ class Dataset(MotionDataset):
 
     def prepare_data(self):
         """create a list of Trajectories from config variables"""
+        np.random.seed(self.config.seed)
         train_trajectories = [
             self.generate_traj(i)
             for i in tqdm(range(int(self.config.num_traj * self.config.ratio_train)))
@@ -101,9 +101,9 @@ class Dataset(MotionDataset):
         )
 
     def get_random_target(self) -> List[float]:
-        x = random.uniform(self.config.min_x, self.config.max_x)
-        y = random.uniform(self.config.min_y, self.config.max_y)
-        z = random.uniform(self.config.min_z, self.config.max_z)
+        x = np.random.uniform(self.config.min_x, self.config.max_x)
+        y = np.random.uniform(self.config.min_y, self.config.max_y)
+        z = np.random.uniform(self.config.min_z, self.config.max_z)
         random_euler = list(R.random().as_euler(SEQ))  # zyx
         return np.array([x, y, z] + random_euler)
 

prescyent/predictor/base_predictor.py
@@ -63,7 +63,10 @@ class BasePredictor:
         self.name = self.config.name
         self.version = self.config.version
         self._init_logger(no_sub_dir_log)
-        self._init_scaler()
+        if (
+            not hasattr(self, "scaler") or self.scaler is None
+        ):  # If scaler wasn't _init by child
+            self._init_scaler()
         logger.getChild(PREDICTOR).info(self.describe())
 
     def describe(self):
@@ -133,6 +136,7 @@ class BasePredictor:
         if self.scaler:
             logger.getChild(PREDICTOR).info("Training Scaler")
             self.train_scaler(datamodule)
+            logger.getChild(PREDICTOR).info("Scaler Trained")
 
     def train_scaler(self, datamodule: LightningDataModule):
         """_summary_
@@ -140,13 +144,12 @@ class BasePredictor:
         Args:
             datamodule (_type_): _description_
         """
+        # cat all dataset's frames
+        dataset_tensor = torch.cat(
+            [traj.tensor for traj in datamodule.trajectories.train], dim=0
+        )
         self.scaler.train(
-            DataLoader(
-                torch.cat(
-                    [traj.tensor for traj in datamodule.trajectories.train], dim=0
-                ),
-                batch_size=datamodule.config.batch_size,
-            ),
+            DataLoader(dataset_tensor, batch_size=datamodule.config.batch_size),
             dataset_features=datamodule.tensor_features,
         )
 

prescyent/predictor/lightning/module.py
@@ -69,6 +69,7 @@ class LightningModule(pl.LightningModule):
     def __init__(self, torch_model_class: Type[BaseTorchModule], config) -> None:
         logger.getChild(PREDICTOR).info("Initialization of the Lightning Module...")
         super().__init__()
+        self.config = config
         self.lr = 0.999
         self.torch_model = torch_model_class(config)
         self.val_output = []
@@ -142,19 +143,33 @@ class LightningModule(pl.LightningModule):
             return [optimizer], [{"scheduler": lr_scheduler, "interval": "step"}]
         return [optimizer]
 
-    def get_metrics(self, batch, prefix: str = "", loss_only=False):
-        """get loss and accuracy metrics from batch"""
+    def predict(self, batch):
+        """get prediction from batch and model"""
         sample, truth = batch
-        sample = self.scaler.scale(sample)
-        truth = self.scaler.scale(truth)
+        if self.scaler:
+            sample = self.scaler.scale(
+                sample,
+                self.config.dataset_config.in_points,
+                self.config.dataset_config.in_features,
+            )
         pred = self.torch_model(sample)
+        if self.scaler:
+            pred = self.scaler.unscale(
+                pred,
+                self.config.dataset_config.out_points,
+                self.config.dataset_config.out_features,
+            )
         if torch.any(torch.isnan(pred)):
             logger.getChild(PREDICTOR).error("NAN in pred")
-            # raise ValueError("Please check your loss function and architecture")
+            raise ValueError("Please check your loss function and architecture")
+        return pred, truth
+
+    def compute_metrics(self, pred, truth, prefix: str = "", loss_only=False):
+        """get loss and accuracy metrics pred and truth"""
         loss = self.criterion(pred, truth)
         if torch.any(torch.isnan(loss)):
             logger.getChild(PREDICTOR).error("NAN in loss")
-            # raise ValueError("Please check your loss function and architecture")
+            raise ValueError("Please check your loss function and architecture")
         self.log(f"{prefix}/loss", loss.detach(), prog_bar=True)
         if loss_only:
             return {"loss": loss}
@@ -227,7 +242,8 @@ class LightningModule(pl.LightningModule):
     def training_step(self, *args, **kwargs):
         """run every training step"""
         batch = args[0]
-        res = self.get_metrics(batch, "Train", loss_only=True)
+        pred, truth = self.predict(batch)
+        res = self.compute_metrics(pred, truth, "Train", loss_only=True)
         self.train_output.append(res)
         return res
 
@@ -235,7 +251,8 @@ class LightningModule(pl.LightningModule):
         """run every test step"""
         with torch.no_grad():
             batch = args[0]
-            res = self.get_metrics(batch, "Test")
+            pred, truth = self.predict(batch)
+            res = self.compute_metrics(pred, truth, "Test")
             self.test_output.append(res)
             return res
 
@@ -243,7 +260,8 @@ class LightningModule(pl.LightningModule):
         """run every validation step"""
         with torch.no_grad():
             batch = args[0]
-            res = self.get_metrics(batch, "Val")
+            pred, truth = self.predict(batch)
+            res = self.compute_metrics(pred, truth, "Val")
             self.val_output.append(res)
             return res
 

prescyent/scaler/scaler.py
@@ -1,4 +1,11 @@
-from typing import List, Optional
+from pathlib import Path
+from typing import List, Optional, Union
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
 
 import torch
 import torch.utils
@@ -15,6 +22,7 @@ from .scaling_methods.normalizer import Normalizer
 norm_map = {
     Scalers.STANDARDIZATION: Standardizer,
     Scalers.NORMALIZATION: Normalizer,
+    None: None,
 }
 
 # TODO: norm_on_last_frame
@@ -56,23 +64,26 @@ class Scaler:
                 raise AttributeError(
                     f"Please choose a valid scaler in NormalizationConfig from the following: {norm_map.keys()}"
                 )
-            self.scalers["feature"] = scaler(self.config.scaling_axis)
-            self.scalers["feature"].train(dataset_dataloader)
+            if scaler is not None and self.config.scaling_axis is not None:
+                self.scalers["feature"] = scaler(self.config.scaling_axis)
+                self.scalers["feature"].train(dataset_dataloader)
         else:
             for feat in dataset_features:
-                if not isinstance(feat, Rotation) or self.config.scale_rotations:
+                if (
+                    not isinstance(feat, Rotation) or self.config.scale_rotations
+                ):  # If its not a Rotation, or if we scale them
                     scaler = norm_map.get(self.config.scaler, -1)
                     if scaler == -1:
                         raise AttributeError(
                             f"Please choose a valid scaler in NormalizationConfig from the following: {norm_map.keys()}"
                         )
-                    self.scalers[feat.name] = scaler(self.config.scaling_axis)
-                    self.scalers[feat.name].train(dataset_dataloader, feat.ids)
+                    if scaler is not None and self.config.scaling_axis is not None:
+                        self.scalers[feat.name] = scaler(self.config.scaling_axis)
+                        self.scalers[feat.name].train(dataset_dataloader, feat.ids)
         self.status = "trained"
         self.dataset_features = dataset_features
         logger.getChild(PREDICTOR).info(f"Trained {self.describe()}")
 
-    @self_auto_batch
     def scale(
         self,
         input_t: torch.Tensor,
@@ -83,10 +94,8 @@ class Scaler:
             raise AssertionError(
                 "Scaler wasn't trained, please train before using method"
             )
-        if not self.scalers:
-            logger.getChild(PREDICTOR).warning(
-                "Scaler wasn't trained, returning input as is"
-            )
+        assert len(input_t.shape) == 4
+        if not self.scalers:  # If we don't have any scaler init
             return input_t
         output_t = input_t.clone()
         if not self.config.do_feature_wise_scaling or features is None:
@@ -94,18 +103,26 @@ class Scaler:
         for feat in features:
             if not isinstance(feat, Rotation) or self.config.scale_rotations:
                 feat_ids = None
-                if feat not in self.dataset_features:
-                    if isinstance(feat, Rotation):
+                if feat not in self.dataset_features:  # If don't find the feature
+                    if isinstance(
+                        feat, Rotation
+                    ):  # we cannot "convert" the feature for a rotation to another
                         raise AttributeError(
                             f"We cannot scale unknown rotation, know feature from dataset training are {self.dataset_features}"
                         )
+                    if not self.scalers.get(
+                        feat.name, None
+                    ):  # we cannot "convert" the feature with a different name
+                        logger.getChild(PREDICTOR).warning(
+                            f"Scaler for feat {feat.name} wasn't trained, skipping this feat"
+                        )
+                        continue
                     feat_ids = feat.ids
                 output_t[..., feat.ids] = self.scalers[feat.name].scale(
                     output_t[..., feat.ids], in_points_ids, feat_ids
                 )
         return output_t
 
-    @self_auto_batch
     def unscale(
         self,
         input_t: torch.Tensor,
@@ -116,10 +133,8 @@ class Scaler:
             raise AssertionError(
                 "Scaler wasn't trained, please train before using method"
             )
+        assert len(input_t.shape) == 4
         if not self.scalers:
-            logger.getChild(PREDICTOR).warning(
-                "Scaler wasn't trained, returning input as is"
-            )
             return input_t
         output_t = input_t.clone()
         if not self.config.do_feature_wise_scaling or features is None:
@@ -138,8 +153,21 @@ class Scaler:
                 )
         return output_t
 
-    def load(self):
-        raise NotImplementedError()
+    def save(self, file_path: Union[str, Path]):
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        if not file_path.parent.exists():
+            file_path.parent.mkdir(parents=True)
+        with file_path.open("wb") as file_wb:
+            pickle.dump(self.__dict__, file_wb, 2)
 
-    def save(self):
-        raise NotImplementedError()
+    @classmethod
+    def load(cls, file_path: Union[str, Path]):
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        with file_path.open("rb") as file_rb:
+            dict_data = pickle.load(file_rb)
+        scaler = cls(dict_data["config"])
+        scaler.__dict__.clear()
+        scaler.__dict__.update(dict_data)
+        return scaler

prescyent/scaler/scaling_methods/standardizer.py
@@ -11,26 +11,32 @@ from prescyent.utils.enums import TrajectoryDimensions
 class Standardizer:
     mean: torch.Tensor
     std: torch.Tensor
-    dim: Optional[List[int]]
+    dim: List[int]
     eps: float
 
-    def __init__(self, scaling_axis: Optional[TrajectoryDimensions], eps=1e-8) -> None:
+    def __init__(self, scaling_axis: TrajectoryDimensions, eps=1e-8) -> None:
         self.dim = list(
             set(json.loads(scaling_axis.value) + [0, 1])
-        )  # Always scale on batch and time_frames
+        )  # + [0, 1] because we scale features, so we at least compute over batch and time_frames
         self.eps = eps
 
     def train(
         self, dataset_dataloader: DataLoader, feat_ids: Optional[List[int]] = None
     ):
+        """Train the standardizer based on a dataloader over the whole dataset and the dataset's features
+
+        Args:
+            dataset_dataloader (DataLoader): Dataloader over the whole training dataset
+            feat_ids (Optional[List[int]], optional): List of the dataset features. Defaults to None.
+        """
         n_samples = 0
         total_sum = 0
         total_sum_sq = 0
         for batch in dataset_dataloader:
             data = batch.unsqueeze(0)
-            sample_size = math.prod([data.size(dim) for dim in self.dim])
             if feat_ids:
                 data = data[..., feat_ids]
+            sample_size = math.prod([data.size(dim) for dim in self.dim])
             n_samples += sample_size
             total_sum += data.sum(dim=self.dim)
             total_sum_sq += (data**2).sum(dim=self.dim)
@@ -45,6 +51,18 @@ class Standardizer:
         point_ids: List[int] = None,
         feat_ids: List[int] = None,
     ) -> torch.Tensor:
+        """Standardizes input tensor
+
+        Args:
+            sample_tensor (torch.Tensor): input tensor
+            point_ids (List[int], optional): list of point ids to standardize on.
+                If None, standardize over whole given points. Defaults to None.
+            feat_ids (List[int], optional): list of feature ids to standardize on.
+                If None, standardize over whole given features. Defaults to None.
+
+        Returns:
+            torch.Tensor: Standardized input tensor
+        """
         if self.dim == [0, 1, 3]:
             sample_tensor = sample_tensor.transpose(2, 3)
         if (
@@ -77,6 +95,18 @@ class Standardizer:
         point_ids: List[int] = None,
         feat_ids: List[int] = None,
     ) -> torch.Tensor:
+        """Unstandardizes input tensor
+
+        Args:
+            sample_tensor (torch.Tensor): input tensor
+            point_ids (List[int], optional): list of point ids to Unstandardize on.
+                If None, Unstandardize over whole given points. Defaults to None.
+            feat_ids (List[int], optional): list of feature ids to Unstandardize on.
+                If None, Unstandardize over whole given features. Defaults to None.
+
+        Returns:
+            torch.Tensor: Unstandardized input tensor
+        """
         if self.dim == [0, 1, 3]:
             sample_tensor = sample_tensor.transpose(2, 3)
         if (

tests/predictor/test_delayed.py
@@ -27,10 +27,7 @@ class DelayedPredictorTests(CustomTestCase):
             out_points=points,
         )
         predictor = DelayedPredictor(
-            PredictorConfig(
-                dataset_config=dataset_config,
-                save_path="tmp"
-                )
+            PredictorConfig(dataset_config=dataset_config, save_path="tmp")
         )
         input_tensor = torch.rand(20, 10, 7, 6)
         output = predictor.predict(input_tensor, len(input_tensor[0]))
@@ -58,10 +55,7 @@ class DelayedPredictorTests(CustomTestCase):
             out_points=points,
         )
         predictor = DelayedPredictor(
-            PredictorConfig(
-                dataset_config=dataset_config,
-                save_path="tmp"
-                )
+            PredictorConfig(dataset_config=dataset_config, save_path="tmp")
         )
         trajs = Trajectories(
             [(Trajectory(torch.rand(500, 9, 7), 10, features)) for i in range(1)],